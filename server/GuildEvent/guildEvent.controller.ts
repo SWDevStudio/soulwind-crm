dcd728fbdaf98ce33f1521bb7366aa13252d7ba7
Kotaro <swdevstudio@gmail.com>
2021-09-06 01:56:50 +0300

    Добавил middleware для guildEvent.controller.ts

--- server/GuildEvent/guildEvent.controller.ts
+++ server/GuildEvent/guildEvent.controller.ts
@@ -1,9 +1,32 @@
 import { Router } from "express"
+import AuthorizeMiddleware from "../middleware/AuthorizeMiddleware"
+import RolesMiddleware from "../middleware/RolesMiddleware"
 import GuildEventService from "./guildEvent.service"
 const GuildEventController = Router()
 
-GuildEventController.get("", GuildEventService.getEvents)
-GuildEventController.get("/:id", GuildEventService.getEvent)
-GuildEventController.post("", GuildEventService.createEvent)
-GuildEventController.patch("/:id", GuildEventService.updateEvent)
+GuildEventController.get(
+  "",
+  AuthorizeMiddleware,
+  RolesMiddleware(["USER", "ADMIN"]),
+  GuildEventService.getEvents
+)
+GuildEventController.get(
+  "/:id",
+  AuthorizeMiddleware,
+  RolesMiddleware(["USER", "ADMIN"]),
+  GuildEventService.getEvent
+)
+GuildEventController.post(
+  "",
+  AuthorizeMiddleware,
+  RolesMiddleware(["ADMIN"]),
+  GuildEventService.createEvent
+)
+GuildEventController.patch(
+  "/:id",
+  AuthorizeMiddleware,
+  RolesMiddleware(["ADMIN"]),
+  GuildEventService.updateEvent
+)
+
 export default GuildEventController
