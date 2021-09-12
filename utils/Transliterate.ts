382be5f89e510ba6efa59e121e35923aab67afe9
Kotaro <swdevstudio@gmail.com>
2021-09-12 04:30:38 +0300

    Добавил тестовый метод транслитора (думаю проще написать свой, этот взял с инета)

---
+++ utils/Transliterate.ts
@@ -0,0 +1,38 @@
+export function Transliterate(word: string): string {
+  const keys: { [key: string]: string } = {
+    а: "a",
+    б: "b",
+    в: "v",
+    г: "g",
+    д: "d",
+    е: "e",
+    ё: "e",
+    ж: "j",
+    з: "z",
+    и: "i",
+    к: "k",
+    л: "l",
+    м: "m",
+    н: "n",
+    о: "o",
+    п: "p",
+    р: "r",
+    с: "s",
+    т: "t",
+    у: "u",
+    ф: "f",
+    х: "h",
+    ц: "c",
+    ч: "ch",
+    ш: "sh",
+    щ: "shch",
+    ы: "y",
+    э: "e",
+    ю: "u",
+    я: "ya",
+  }
+  return word
+    .split("")
+    .map((char) => keys[char] || char)
+    .join("")
+}
