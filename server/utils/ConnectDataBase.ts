3208dbed7a7f9700842d0a3b056ec4cd9129d732
Kotaro <swdevstudio@gmail.com>
2021-09-21 00:55:01 +0300

    swagger открывается теперь только в dev mode

--- server/utils/ConnectDataBase.ts
+++ server/utils/ConnectDataBase.ts
@@ -1,14 +1,13 @@
 import { connect } from "mongoose"
-
+import isDev from "./IsDev"
 export function ConnectDataBase(
   login: string = "KotaroSW",
   passwd: string = "KotaroSW2015",
   db: string = "myFirstDatabase"
 ) {
-  const uri =
-    process.env.NODE_ENV === "development"
-      ? `mongodb+srv://${login}:${passwd}@cluster0.v6u1x.mongodb.net/${db}?retryWrites=true&w=majority`
-      : `mongodb+srv://${login}:${passwd}@cluster0.pes58.mongodb.net/${db}?retryWrites=true&w=majority`
+  const uri = isDev()
+    ? `mongodb+srv://${login}:${passwd}@cluster0.v6u1x.mongodb.net/${db}?retryWrites=true&w=majority`
+    : `mongodb+srv://${login}:${passwd}@cluster0.pes58.mongodb.net/${db}?retryWrites=true&w=majority`
   connect(uri, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
